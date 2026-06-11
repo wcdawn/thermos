9d3b6d176214f8d09301688a65e1074c7af53c84
William Dawn <wdawn730@gmail.com>
2026-06-11 14:56:50 -0600

    notes mostly... doesn't work!

--- src/geometry.f90
+++ src/geometry.f90
@@ -13,6 +13,19 @@ contains
     integer(ik), intent(in) :: nx
     real(rk), intent(out) :: xcenter(:) ! Center coordinate for cell [cm]
     real(rk), intent(out) :: dx(:) ! Cell-width [cm]
+    
+    real(rk) :: h
+    integer(ik) :: i
+
+    ! NOTE: For now, this calculates uniformly spaced coordinates regardless of
+    ! the geometry. In the future, it would be nice to support equal-area
+    ! (cylindrical) and equal-volume (spherical) coordiantes for other
+    ! geometries.
+
+    h = length / nx
+    dx = h
+    ! TODO fill xcenter
+
   endsubroutine geometry_calculate_coordinates
 
 endmodule geometry
