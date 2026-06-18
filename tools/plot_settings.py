96e40da43aa444ceaf19a7006dee75f6deff7bbe
William Dawn <wdawn730@gmail.com>
2026-06-18 11:10:55 -0600

    adding plotting tools for temperature

---
+++ tools/plot_settings.py
@@ -0,0 +1,6 @@
+import matplotlib
+
+matplotlib.rcParams["lines.linewidth"] = 2
+# matplotlib.rcParams["mathtext.fontset"] = "stix"
+# matplotlib.rcParams["font.family"] = "STIXGeneral"
+matplotlib.rcParams["font.size"] = 14
